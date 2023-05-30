38fc5860ba9c0db8b7d085d26fd30202c4cad313
hoangbao301099 <63846180+hoangbao301099@users.noreply.github.com>
2023-05-30 17:20:59 +0700

    setup front-end

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './redux/slices/Couterslice'
+import styled from 'styled-components'
+
+function App() {
+	const count = useSelector((state) => state.counter.value)
+	const dispatch = useDispatch()
+
+	const Button = styled.button({
+		backgroundColor: 'red',
+	})
+
+	return (
+		<div>
+			<div>
+				<Button
+					aria-label='Increment value'
+					onClick={() => dispatch(increment())}
+				>
+					Increment
+				</Button>
+				<span>{count}</span>
+				<button
+					aria-label='Decrement value'
+					onClick={() => dispatch(decrement())}
+				>
+					Decrement
+				</button>
+			</div>
+		</div>
+	)
+}
+export default App
